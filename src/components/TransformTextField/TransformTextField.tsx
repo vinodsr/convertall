706562a794d3a8a2e4b708cfd1f3993f54a75c47
Vinod S R <smartvinu@gmail.com>
2022-01-10 18:57:22 +0530

    Add time lag for event emission
    Closes #1

--- src/components/TransformTextField/TransformTextField.tsx
+++ src/components/TransformTextField/TransformTextField.tsx
@@ -13,17 +13,33 @@ interface TransformTextFieldProps {
  * Component to accept the transform text
  */
 export default function TransformTextField(props: TransformTextFieldProps) {
+  let onChangeTimeout: any = null;
+  /**
+   * Trigger text change
+   *
+   * Will do a timeout for 150 to prevent processing brust
+   *
+   * @param {string} text
+   */
+  const triggerOnchange = (text: string) => {
+    if (onChangeTimeout !== null) {
+      clearTimeout(onChangeTimeout);
+    }
+    onChangeTimeout = setTimeout(() => {
+      props.onChange(text);
+    }, 150);
+  };
+
   return (
     <TextField
       id="text"
       label="Text to transform"
       variant="outlined"
       sx={{ width: 3 / 4 }}
-      value={props.text}
       autoFocus
       multiline={true}
       onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-        props.onChange(event.currentTarget.value);
+        triggerOnchange(event.currentTarget.value);
       }}
     />
   );
